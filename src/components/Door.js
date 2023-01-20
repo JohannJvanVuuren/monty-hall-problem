baab5caf2d1fff97d56389c7d5c2c4ee5872c93c
Johann JvanVuuren <technomagekomt@gmail.com>
2023-01-20 05:43:16 +0200

    22/01/20 05:42: Individual door open functionality done.

--- src/components/Door.js
+++ src/components/Door.js
@@ -1,28 +1,21 @@
 import '../scss/main.css';
+// import noTreasure from '../scss/images/no-treasure.jpg'
+import potOfGold from '../scss/images/goldpot.jpg';
 
 
+const Door = ({isActive, onShow, revealTreasure}) => {
 
+    const doorOpen = isActive ? "door-closed open" : "door-closed";
 
-
-const Door = ({doorId}) => {
-
-    const additionalClass = "";
-    const doorClosed = `door-closed + ${additionalClass}`;
-
-    const onClickHandler = () => {
-        doorClosed.classList.add("open");
-    }
 
     return (
         <div className={"door"}>
-            <div className={doorClosed} onClick={onClickHandler}>
-                <div className={"door-knob"}></div>
-                <div className={"door-open"}>
-                    <div>
-                        {}
-                    </div>
-                </div>
+
+            <img className={"image-win"} src={potOfGold} alt={"Pot of gold"}/>
+            <div className={doorOpen} onClick={onShow}>
+                <div className={"door-knob"} ></div>
             </div>
+
         </div>
     )
 }
--- src/components/Hall.js
+++ src/components/Hall.js
@@ -4,19 +4,35 @@ import Door from "./Door";
 import '../scss/main.css';
 const Hall = () => {
 
+    const [activeIndex, setActiveIndex] = useState(0);
+
     // const [treasure, setTreasure] = useState(false);
 
     //Generates random numbers between 1 and 3 (incl) to determine where the treasure will be hidden
-    // const generateRandomNumber = () => {
-    //     return (Math.random() * 3) + 1;
-    // }
+    const generateRandomNumber = () => {
+        return (Math.random() * 3) + 1;
+    }
+
+    const treasureDoor = generateRandomNumber();
 
 
     return (
         <div className={"hall"}>
-            <Door />
-            <Door />
-            <Door />
+            <Door
+                isActive={activeIndex === 0}
+                onShow={() => setActiveIndex(0)}
+                revealTreasure={activeIndex === treasureDoor}
+            />
+            <Door
+                isActive={activeIndex === 1}
+                onShow={() => setActiveIndex(1)}
+                revealTreasure={activeIndex === treasureDoor}
+            />
+            <Door
+                isActive={activeIndex === 2}
+                onShow={() => setActiveIndex(2)}
+                revealTreasure={activeIndex === treasureDoor}
+            />
         </div>
     )
 
