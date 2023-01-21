79cffe8e59d1e7fb55e04f39b82984bbaa9325d9
Johann JvanVuuren <technomagekomt@gmail.com>
2023-01-21 09:31:23 +0200

    22/01/21 09:30: Single and double click set up on doors to select or reveal respectively.

--- src/components/Door.js
+++ src/components/Door.js
@@ -3,22 +3,27 @@ import noTreasure from '../scss/images/no-treasure.jpg'
 import potOfGold from '../scss/images/goldpot.jpg';
 
 
-const Door = ({isActive, onShow, revealTreasure, doorId}) => {
+const Door = ({isActive, onShow, revealTreasure, doorId, onSelect}) => {
 
+    /* Using the prop isActive to decide which door to open. */
     const doorOpen = isActive ? "door-closed open" : "door-closed";
 
+    const handleFirstClick = () => {
 
+    }
 
     return (
         <div className={"door"}>
 
-
+            {/* Ternary operator using doorId and revealTreasure props to determine which image to display
+            (win or lose) */}
             {doorId === revealTreasure ? (
                 <img className={"image-win show-image"} src={potOfGold} alt={"Pot of gold"} />
             ) : (
                 <img className={"image-lose show-image"} src={noTreasure} alt={"No treasure red cross"} />
             )}
-            <div className={doorOpen} onClick={onShow}>
+
+            <div className={doorOpen} onClick={onSelect} onDoubleClick={onShow}>
                 <div className={"door-knob"} ></div>
             </div>
 
--- src/components/Hall.js
+++ src/components/Hall.js
@@ -13,27 +13,36 @@ const Hall = () => {
         return Math.floor(Math.random() * 3) + 1;
     }
 
-    let treasureDoor = generateRandomNumber();
 
+    let treasureDoor = generateRandomNumber();
 
     return (
         <div className={"hall"}>
+            {/* Properties set to transfer data to Door child component. doorId is to enable a way to determine
+            which door has the treasure behind it. This is done by corresponding this number to the random number
+            generated using generateRandomNumber(). isActive is to determine which door to open and onShow sets the
+            state variable activeIndex that enables isActive. revealTreasure is used together with doorId to decide
+            where the treasure image is to be displayed in the Door component.
+            */}
             <Door
                 doorId={1}
-                isActive={activeIndex === 0}
-                onShow={() => setActiveIndex(0)}
-                revealTreasure={treasureDoor}
-            />
-            <Door
-                doorId={2}
                 isActive={activeIndex === 1}
+                onSelect={() => console.log(`Door 1 selected.`)}
                 onShow={() => setActiveIndex(1)}
                 revealTreasure={treasureDoor}
             />
             <Door
-                doorId={3}
+                doorId={2}
                 isActive={activeIndex === 2}
+                onSelect={() => console.log(`Door 2 selected.`)}
                 onShow={() => setActiveIndex(2)}
+                revealTreasure={treasureDoor}
+            />
+            <Door
+                doorId={3}
+                isActive={activeIndex === 3}
+                onSelect={() => console.log(`Door 3 selected.`)}
+                onShow={() => setActiveIndex(3)}
                 revealTreasure={treasureDoor}
             />
         </div>
